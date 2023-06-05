c93763380040abf145ffaf0ed028cca614c683cf
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-05 19:22:59 +0800

    Add initial app type interfaces

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,5 @@
+import { Types } from 'mongoose';
+
 export {};
 
 declare global {
@@ -10,5 +12,46 @@ declare global {
 
   interface ResponseError extends Error {
     status?: number;
+  }
+
+  interface BlogPost {
+    _id: string;
+    date_created: Date | string;
+    title: string;
+    slug: string;
+    author: Types.ObjectId; // user id
+    content: string;
+    comments?: Comment[];
+    liked_by?: Types.ObjectId[]; // user ids
+    tags?: Types.ObjectId[]; // tag ids
+    edits?: {
+      date_created: Date | string;
+    }[];
+  }
+
+  interface Comment {
+    _id: string;
+    date_created: Date | string;
+    author: Types.ObjectId;
+    content: string;
+    comment_level: number;
+    parent_comment_id?: Types.ObjectId;
+    liked_by: Types.ObjectId[]; // user ids
+  }
+
+  interface User {
+    _id: string;
+    date_created: Date | string;
+    username: string;
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    posts: Types.ObjectId[]; // post ids
+  }
+
+  interface Tag {
+    _id: string;
+    name: string;
   }
 }
