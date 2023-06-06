7214e55070f30cb2416e76e2afe85e0daa7df923
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-06 22:03:58 +0800

    Add initial api route handlers

--- routes/api.ts
+++ routes/api.ts
@@ -1,3 +1,28 @@
-import express, { Request, Response, NextFunction } from 'express';
-import asyncHandler from 'express-async-handler';
-import { body, validationResult } from 'express-validator';
+import express, { Request, Response } from 'express';
+
+import userController from '../controllers/userController';
+
+const router = express.Router();
+
+/**
+ * ------------- API HOME ROUTE -------------
+ */
+
+router.get('/', (req: Request, res: Response) => {
+  return res.json({ message: 'Welcome to the API' });
+});
+
+/**
+ * ------------- USER ROUTES -------------
+ */
+
+// GET request for list of all users
+router.get('/users', userController.user_list);
+
+// POST request for creating new user
+router.post('/users', userController.user_create);
+
+// GET request for a specific user
+router.get('/users/:id', (req: Request, res: Response) => {});
+
+export default router;
