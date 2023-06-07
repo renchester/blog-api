717dc4249d864a128b0e0aa344e1dad542f001ad
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-07 23:19:46 +0800

    Create Passport JWT strategy module

---
+++ lib/passportJWT.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  StrategyOptions,
+} from 'passport-jwt';
+import User from '../models/user';
+import passport from 'passport';
+
+dotenv.config();
+
+const PUB_KEY = process.env.PUB_KEY;
+
+const options: StrategyOptions = {
+  // Format --> Bearer <Token>
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256'],
+};
+
+const strategy = new JwtStrategy(options, async (payload, done) => {
+  try {
+    const userId = payload.sub;
+
+    const user = await User.findById(userId);
+
+    // JWT is already validated, check if user is in database and return to passport
+    // Passport JWT takes the jwt from the header and validates it with the jsonwebtoken library
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: 'Unable to find user' });
+    }
+  } catch (err) {
+    done(err, false, { message: 'Authentication failed' });
+  }
+});
+
+passport.use(strategy);
