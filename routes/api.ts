30f8a1c1c8ef681bedd12b7b44af9387f33fd341
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-07 17:58:24 +0800

    Add controller functions to api route handler

--- routes/api.ts
+++ routes/api.ts
@@ -17,12 +17,21 @@ router.get('/', (req: Request, res: Response) => {
  */
 
 // GET request for list of all users
-router.get('/users', userController.user_list);
+router.get('/users', userController.get_users);
 
 // POST request for creating new user
-router.post('/users', userController.user_create);
+router.post('/users', userController.create_user);
 
 // GET request for a specific user
-router.get('/users/:id', (req: Request, res: Response) => {});
+router.get('/users/:id', userController.get_user_by_id);
+
+// PUT request for updating user details
+router.put('/users/:id', userController.update_details);
+
+// PATCH request for updating user password
+router.patch('/users/:id', userController.update_password);
+
+// DELETE request for deleting user
+router.delete('/users/:id', userController.delete_user);
 
 export default router;
