5f357aee8e74088b6d0a22ae5fcb1fcd2c9b1d65
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-08 22:58:59 +0800

    Add api route endpoints and handlers

--- routes/api.ts
+++ routes/api.ts
@@ -3,7 +3,8 @@ import express, { Request, Response } from 'express';
 import userController from '../controllers/userController';
 import postController from '../controllers/postController';
 import tagController from '../controllers/tagController';
-import authenticateJWT from '../lib/authenticateJWT';
+import commentController from '../controllers/commentController';
+import { authenticateJWT, retrieveUserFromJWT } from '../lib/authHandlers';
 
 const router = express.Router();
 
@@ -29,13 +30,28 @@ router.post('/users', userController.create_user);
 router.get('/users/:id', userController.get_user_by_id);
 
 // PUT request for updating user details
-router.put('/users/:id', userController.update_details);
+router.put(
+  '/users/:id',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  userController.update_details,
+);
 
 // PATCH request for updating user password
-router.patch('/users/:id/password', userController.update_password);
+router.patch(
+  '/users/:id/password',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  userController.update_password,
+);
 
 // DELETE request for deleting user
-router.delete('/users/:id', userController.delete_user);
+router.delete(
+  '/users/:id',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  userController.delete_user,
+);
 
 /**
  * ------------- POST ROUTES -------------
@@ -45,19 +61,68 @@ router.delete('/users/:id', userController.delete_user);
 router.get('/posts', postController.get_posts); //include query by tag
 
 // POST request to create new post
-router.post('/posts', authenticateJWT, postController.create_post);
+router.post(
+  '/posts',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  postController.create_post,
+);
 
 // GET request to retrieve post
 router.get('/posts/:id', postController.get_post_by_id);
 
 // PATCH request to update post body
-router.patch('/posts/:id/content', authenticateJWT, postController.edit_post);
-
-// GET request to retrieve post comments
-router.get('/posts/:id/comments', postController.get_post_comments);
+router.patch(
+  '/posts/:id/content',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  postController.edit_post,
+);
 
 // DELETE request for deleting post
-router.delete('/posts/:id', authenticateJWT, postController.delete_post);
+router.delete(
+  '/posts/:id',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  postController.delete_post,
+);
+
+/**
+ * ------------- COMMENT ROUTES -------------
+ */
+
+// GET request to retrieve post comments
+router.get('/posts/:postid/comments', commentController.get_post_comments);
+
+// POST request to add new comment
+router.post(
+  '/posts/:postid/comments',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  commentController.create_comment,
+);
+
+// GET request to retrieve a specific comment
+router.get(
+  '/posts/:postid/commens/:commentid',
+  commentController.get_comment_by_id,
+);
+
+// PATCH request to update comment body
+router.patch(
+  '/posts/:postid/comments',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  commentController.edit_comment,
+);
+
+// DELETE request to delete comment
+router.delete(
+  '/posts/:postid/commens/:commentid',
+  authenticateJWT,
+  retrieveUserFromJWT,
+  commentController.delete_comment,
+);
 
 /**
  * ------------- TAG ROUTES -------------
