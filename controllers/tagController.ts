b0174f25d069f37a495a69606c3e5b8a597cbaba
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-10 01:17:25 +0800

    Add middleware to edit tag

--- controllers/tagController.ts
+++ controllers/tagController.ts
@@ -6,14 +6,7 @@ import Tag from '../models/tag';
 import BlogPost from '../models/blogPost';
 
 const tagController = (() => {
-  const get_tags = asyncHandler(async (req, res, next) => {
-    const tags = await Tag.find({});
-
-    res.json({ tags });
-  });
-
-  const create_tag = [
-    // Validate and sanitize fields
+  const validateTagName = () =>
     body('name', 'Tag name must be between 3 and 64 characters')
       .trim()
       .isLength({ min: 3, max: 64 })
@@ -27,7 +20,17 @@ const tagController = (() => {
         if (isTagNameTaken) {
           throw new Error('Tag name is already in use');
         }
-      }),
+      });
+
+  const get_tags = asyncHandler(async (req, res, next) => {
+    const tags = await Tag.find({}, { __v: 0 });
+
+    res.json({ tags });
+  });
+
+  const create_tag = [
+    // Validate and sanitize fields
+    validateTagName(),
 
     // Process request after validation and sanitization
     asyncHandler(async (req, res, next) => {
@@ -62,7 +65,7 @@ const tagController = (() => {
   ];
 
   const get_tag_by_id = asyncHandler(async (req, res, next) => {
-    const tag = await Tag.findById(req.params.id);
+    const tag = await Tag.findById(req.params.id, { __v: 0 });
 
     if (tag === null) {
       const err = createError(400, 'Tag not found');
@@ -72,9 +75,57 @@ const tagController = (() => {
     res.json({ tag });
   });
 
+  const edit_tag = [
+    // Validate and sa?nitize fields
+    validateTagName(),
+
+    // Process request after validation and sanitization
+    asyncHandler(async (req, res, next) => {
+      // Check if user is admin (has authorization to delete tag)
+      const isAdmin = req.user?.admin;
+
+      if (!isAdmin) {
+        const err = createError(401, 'Unauthorized to edit tag');
+        return next(err);
+      }
+
+      // Extract the validation errors from request
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        // There are errors. Return error message
+        const err = createError(400, errors.array()[0].msg);
+        return next(err);
+      }
+
+      // No errors. Update the record
+      const tag = await Tag.findByIdAndUpdate(
+        req.params.id,
+        { name: req.body.name },
+        { returnDocument: 'after', runValidators: true },
+      );
+
+      if (tag) {
+        // Return appropriate response
+        res
+          .status(201)
+          .location(`/api/tags/${tag._id}`)
+          .json({
+            success: true,
+            message: 'Successfully created tag',
+            tag,
+            link: `/api/tags/${tag._id}`,
+          });
+      } else {
+        const err = createError(500, 'Something went wrong');
+        return next(err);
+      }
+    }),
+  ];
+
   const delete_tag = asyncHandler(async (req, res, next) => {
     // Check if user is admin (has authorization to delete tag)
-    const isAdmin = req.user?.isAdmin;
+    const isAdmin = req.user?.admin;
 
     if (!isAdmin) {
       const err = createError(401, 'Unauthorized to delete tag');
@@ -102,6 +153,7 @@ const tagController = (() => {
     get_tags,
     create_tag,
     get_tag_by_id,
+    edit_tag,
     delete_tag,
   };
 })();
