b25d40f43d15bd95ae026593d845afb8fbc3067b
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-10 01:21:23 +0800

    Update global types

--- types/global.d.ts
+++ types/global.d.ts
@@ -7,13 +7,23 @@ declare global {
     interface ProcessEnv {
       DB_CONNECTION_URL: string;
       SESSION_SECRET: string;
+      PRIV_KEY: string;
+      PUB_KEY: string;
       PORT?: string;
     }
   }
 
   namespace Express {
     interface User {
+      _id: Types.ObjectId;
       username: string;
+      email: string;
+      first_name: string;
+      last_name: string;
+      admin?: boolean;
+    }
+
+    interface Post {
       _id: Types.ObjectId;
     }
   }
@@ -24,39 +34,43 @@ declare global {
 
   interface BlogPost {
     _id: Types.ObjectId;
-    date_created: Date | string;
+    date_created: Date | string | number;
     title: string;
     slug: string;
     author: Types.ObjectId; // user id
+    editors: Types.ObjectId[]; // user ids
     content: string;
-    comments?: Comment[];
-    liked_by?: Types.ObjectId[]; // user ids
-    tags?: Types.ObjectId[]; // tag ids
-    edits?: {
-      date_created: Date | string;
+    comments: Types.DocumentArray<Comment>;
+    liked_by: Types.ObjectId[]; // user ids
+    tags: Types.ObjectId[]; // tag ids
+    edits: {
+      timestamp: Date | string | number;
     }[];
   }
 
   interface Comment {
     _id: Types.ObjectId;
-    date_created: Date | string;
+    date_created: Date | string | number;
     author: Types.ObjectId;
     content: string;
     comment_level: number;
     parent_comment_id?: Types.ObjectId;
     liked_by: Types.ObjectId[]; // user ids
+    edits: {
+      timestamp: Date | string | number;
+    }[];
   }
 
   interface User {
     _id: Types.ObjectId;
-    date_created: Date | string;
+    date_created: Date | string | number;
     username: string;
     email: string;
     hash: string;
     salt: string;
     first_name: string;
     last_name: string;
-    posts: Types.ObjectId[]; // post ids
+    admin?: boolean;
   }
 
   interface Tag {
