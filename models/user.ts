a75a578757cc349f479b6723daa911c49efbcf4d
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-09 23:14:00 +0800

    Set unique property for username and email fields

--- models/user.ts
+++ models/user.ts
@@ -4,8 +4,14 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema<User, Model<User>>({
   date_created: { type: Date, default: Date.now() },
-  username: { type: String, required: true, minLength: 6, maxLength: 30 },
-  email: { type: String, required: true, maxlength: 1024 },
+  username: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 30,
+    unique: true,
+  },
+  email: { type: String, required: true, maxlength: 1024, unique: true },
   salt: { type: String, required: true },
   hash: { type: String, required: true },
   first_name: { type: String, required: true },
