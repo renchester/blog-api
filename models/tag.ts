a5ea7031ef1b15d4a5d4e2ed2b0c3c2ea6078ef8
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-10 15:29:54 +0800

    Add unique property to tag name

--- models/tag.ts
+++ models/tag.ts
@@ -3,7 +3,13 @@ import mongoose, { Model } from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TagSchema = new Schema<Tag, Model<Tag>>({
-  name: { type: String, required: true, minlength: 3, maxlength: 64 },
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 64,
+    unique: true,
+  },
 });
 
 const TagModel = mongoose.model('Tag', TagSchema);
