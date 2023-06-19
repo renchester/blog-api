8ec476a0b6c51862b833c9f1f0952eb84e10c8d2
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-20 06:03:44 +0800

    Add functions to issue access and refresh tokens

---
+++ utils/issueAccessToken.ts
@@ -0,0 +1,41 @@
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PRIV_KEY = process.env.PRIV_ACCESS_KEY;
+
+/**
+ * @param {*} user - The user object.  We need this to set the JWT `sub` payload property to the MongoDB user ID
+ */
+
+const issueAccessToken = (user: User, isNewLogin: boolean) => {
+  const { _id, username, email, first_name, last_name, admin } = user;
+
+  const flag = isNewLogin ? 'login' : 'refresh';
+
+  // Access token has a lifetime of 10mins
+  const expiresIn = '10mins';
+
+  const payload = {
+    sub: _id,
+    user: {
+      _id,
+      username,
+      email,
+      first_name,
+      last_name,
+      admin,
+    },
+    flag,
+  };
+
+  const signedToken = jsonwebtoken.sign(payload, PRIV_KEY, {
+    expiresIn: expiresIn,
+    algorithm: 'RS256',
+  });
+
+  return signedToken;
+};
+
+export default issueAccessToken;
---
+++ utils/issueRefreshToken.ts
@@ -0,0 +1,38 @@
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PRIV_KEY = process.env.PRIV_REFRESH_KEY;
+
+/**
+ * @param {*} user - The user object.  We need this to set the JWT `sub` payload property to the MongoDB user ID
+ */
+
+const issueRefreshToken = (user: User) => {
+  const { _id, username, email, first_name, last_name, admin } = user;
+
+  // Refresh token has a lifetime of 30days
+  const expiresIn = '30d';
+
+  const payload = {
+    sub: _id,
+    user: {
+      _id,
+      username,
+      email,
+      first_name,
+      last_name,
+      admin,
+    },
+  };
+
+  const signedToken = jsonwebtoken.sign(payload, PRIV_KEY, {
+    expiresIn: expiresIn,
+    algorithm: 'RS256',
+  });
+
+  return signedToken;
+};
+
+export default issueRefreshToken;
