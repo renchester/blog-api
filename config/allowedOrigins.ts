d53e0a0900fff0749890471101a6402360f5d201
Renchester Ramos <renchesterjramos@gmail.com>
2023-06-30 11:31:57 +0800

    Update whitelisted origins for CORS requests

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,6 +1,9 @@
 const allowedOrigins = [
   'http://localhost:4000',
+  'http://localhost:3000',
   'https://dezien-blog-api.onrender.com',
+  'https://dezien-blog-p97lc9daf-renchester.vercel.app',
+  'https://dezien-blog.vercel.app',
 ];
 
 export default allowedOrigins;
